18bc0ced9879f43c92677e055229c75f2f7f18c4
Владимир Вислович <vislovichv@mail.ru>
2016-05-24 04:01:26 +0300

    25.06.16 12:10

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
 
 import vk, time
 
+def getPhoto(owner):
+    return  'photo' +str(owner) + '_' + str(api.photos.get(owner_id = owner, album_id = -6, rev=1)[0]['pid'])
 
 #моя страница 244728879
 
@@ -14,7 +16,7 @@ for i in range (1,len(api.users.getNearby(latitude = 55, longitude  = 36, radius
     time.sleep(0.70)
 
 for i in range (1,len(api.users.getNearby(latitude = 55, longitude  = 36, radius = 4, fields = ['photo_id']))):
-    att+='photo' + api.users.getNearby(latitude = 55, longitude  = 36, radius = 4, fields = ['photo_id'])[i]['photo_id'] + ','
+    att+=str(getPhoto(api.users.getNearby(accuracy = 5, latitude = 55, longitude  = 36, radius = 4)[i]['uid'])) + ','
     time.sleep(0.70)
     s+=api.users.getNearby(accuracy = 5, latitude = 55, longitude  = 36, radius = 4)[i]['first_name'] + " "
     time.sleep(0.70)
@@ -26,4 +28,3 @@ print(att)
 
 api.wall.post(owner_id = 244728879 ,friends_only = 0, message = "Относительно рядом со мной \n " + s, v='5.35', attachments = att)
 #print(api.photos.get(owner_ids = 244728879, album_id = -15, v='5.52'))
-print(api.users.get(user_ids = 244728879, fields = ['photo_id']))
