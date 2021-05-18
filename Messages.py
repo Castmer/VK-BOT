e689f0eeb0f29b6b71a2640afd5fbdf005f7b464
Кукушкин Данила <kuk-dan@list.ru>
2021-05-18 20:45:10 +0300

    Update Messages.py

--- Messages.py
+++ Messages.py
@@ -1,4 +1,4 @@
 import vk
 
-session = vk.Session(access_token="b7690ac26382a9b074c4eb953e39a9ef0fb42c0e441df4813046ce1fbbae4d209b55f0af0f895844e8a6d")
+session = vk.Session(access_token="")
 api = vk.API(session)
