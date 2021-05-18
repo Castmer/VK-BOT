c478e9ee71134031b090b74c8b9e7ee70a99c163
Кукушкин Данила <kuk-dan@list.ru>
2021-05-18 20:45:39 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ def getPhoto(owner):
 
 s = ""
 att = ""
-session = vk.Session(access_token="b7690ac26382a9b074c4eb953e39a9ef0fb42c0e441df4813046ce1fbbae4d209b55f0af0f895844e8a6d")
+session = vk.Session(access_token="")
 api = vk.API(session)
 
 for i in range (1,len(api.users.getNearby(latitude = 55, longitude  = 36, radius = 4, fields = ['photo_id']))):
